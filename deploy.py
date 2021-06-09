a4058f9ee2470c67c5756a8cefd607a6a87165e3
Yokmp <yokmp.sf@gmail.com>
2021-06-09 07:58:34 +0200

    args, filesize
    aded some arguments
    -d bool to override the deploy setting
    -s to be less verbose (sum of files only)

--- deploy.py
+++ deploy.py
@@ -12,13 +12,13 @@ source: https://github.com/Yokmp/Factorio-Scripts
 author: Yokmp
 """
 
-import os ,sys, shutil, zipfile, json, platform
+import os, sys, shutil, zipfile, json, platform, getopt
 
 #region ----------------------------------- Boring Settings
-deploy_mod  = True       # create a zip file or not
-
+deploy_mod  = True      # create a zip file or not
+silent      = False     # do not print anything
 ## Blacklist
-exclude     = ["vscode", ".git", ".py", "workspace", ".xcf", "_release_"]
+exclude     = ["_release_", "vscode", "workspace", ".git", ".py", ".xcf"]
 
 ## Get information from filesystem
 workspace   = "."
@@ -30,7 +30,7 @@ version     = ""
 
 ## Path settings - deploy_dir should end in factorio/mods so a new version can be tested easily
 if platform.system() == "Windows":
-  deploy_dir = os.path.join(user_dir, "AppData", "Roaming", "Factorio", "mods")
+  deploy_dir = os.path.join("F:\\", "Games", "Factorio_ModTest", "mods") # os.path.join(user_dir, "AppData", "Roaming", "Factorio", "mods")
 else:
   deploy_dir = os.path.join(user_dir, ".factorio", "mods")
 #endregion -----------------------------------
@@ -58,9 +58,6 @@ if (version == ""):
   print("\nNo version found. Aborting.")
   sys.exit(-1)
 
-text = " Generating files for: " + mod_title + " - version: " + version + " "
-print("\n"+text.center(len(text)+22, "-"))
-
 ## ----------------------------------- Secret Settings
 mod_name_full   = mod_name + "_" + version
 mod_deploy_dir  = os.path.join(deploy_dir, mod_name_full)
@@ -69,36 +66,75 @@ zip_file_path   = os.path.join(workspace, "_release_")
 zip_file        = os.path.join(zip_file_path, mod_name + "_" + version+".zip")
 zip_temp_dir    = os.path.join(workspace, mod_name_full)
 ## -----------------------------------
+## Arguments
+arg = sys.argv[1:]
+try:
+  opts, args = getopt.getopt(arg,"sd:")
+except getopt.GetoptError:
+  print("Usage:\t%s -d:bool -s" %sys.argv[0])
+  print("%s -d\tbool\t - Deploy Mod or not " %sys.argv[0])
+  print("%s -s\t\t - silent: don't show processed files and override any existing file without prompt" %sys.argv[0])
+  sys.exit(2)
+  
+for opt, arg in opts:
+  if opt == "-d":
+    if (arg == "true" or arg == "True"):
+      deploy_mod = True
+    elif (arg == "false" or arg == "False"):
+      deploy_mod = False
+    else: print("%s -d\tbool - Deploy Mod, False will override any existing file without prompt" %sys.argv[0])
+  elif opt == "-s":
+    silent = True
+    
+file_count = sum(len(files) for _, _, files in os.walk(workspace))
+# sys.exit(-1)
+
+text = " Generating files for: " + mod_title + " - version: " + version + " "
+f_size = 0
+print("\n"+text.center(len(text)+22, "-"))
 
 def match_pattern(string):
   for e in exclude:
     if string.find(e) != -1: return True
   return False
+def file_size(_size):
+  power_labels = {0 : '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}; n = 0
+  while _size > 1024: _size /= 1024; n += 1
+  return round(_size, 2), power_labels[n]+'B'
 ## ----------------------------------- COLLECTING
+if silent: i = 0
 for root, subdirs, files in os.walk(workspace):
   if match_pattern(root): continue
-
   for filename in files:
     if match_pattern(filename): continue
 
     file_path = os.path.join(root, filename)
 
     if os.path.isfile(file_path):
-      print('File: %s \t %s' % (mod_name_full, filename))
+      fp = os.path.join(root, filename)
+      f_size += os.path.getsize(fp)
+      if silent: 
+        print("\rCollecting: [{0}/{1}]".format(i,file_count), end='' ); i+=1
+      else:  
+        print('File: %s \t %s' % (mod_name_full, filename))
       os.makedirs(os.path.join(zip_temp_dir, os.path.dirname(file_path[2:])), exist_ok=True)
       shutil.copy(file_path, os.path.join(zip_temp_dir, os.path.dirname(file_path[2:])))
+
+f_size = file_size(f_size)
+print("\rCollected: [{0}] ({1} {2})".format(file_count, f_size[0], f_size[1]))
 
 ## ----------------------------------- ZIPING
 if os.path.exists(zip_file):
   print("\n%s already exists." %zip_file)
-  quest = input("[R]emove or [A]bort? ")
+  quest = input("[R]emove or [A]bort? ") if deploy_mod == True else "r"
   if quest == "r" or quest == "R":
     os.remove(zip_file)
   else:
     shutil.rmtree(zip_temp_dir)
     sys.exit("\n\tScript aborted with '"+quest+"'\n\tNo changes where made.\n")
 
-print('\nCreating\t %s' % zip_name)
+
+print('\nCreating\t %s' %(zip_name), end='')
 zipf = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
 
 for root, subdirs, files in os.walk(zip_temp_dir):
@@ -106,6 +142,10 @@ for root, subdirs, files in os.walk(zip_temp_dir):
     f = os.path.join(root, filename)
     zipf.write(f)
 zipf.close()
+
+z_size = os.path.getsize(zip_name)
+z_size = file_size(z_size)
+print(" ({0} {1})".format(z_size[0], z_size[1]))
 
 shutil.rmtree(zip_temp_dir)
 os.makedirs(zip_file_path, exist_ok=True)
